94275b8ef934fc78477b080e1643c7794ba2cd53
nono <a40376491@126.com>
2025-10-01 21:28:32 +0800

    feat: 恢复主页

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,16 +5,6 @@ const routes = [
   // 兜底 404
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('../views/core/NotFound.vue'), meta: { title: '404 - 页面不存在' } },
   // 具体路由
-  { path: '/', name: 'Home', component: () => import('../views/Home.vue'), meta: { title: '首页 - mirai2cs' } },
-  { path: '/about', name: 'About', component: () => import('../views/About.vue'), meta: { title: '关于 - mirai2cs' } },
-  { path: '/history', name: 'History', component: () => import('../views/History.vue'), meta: { title: '网站日志 - mirai2cs' } },
-  { path: '/cs', name: 'ComputerScience', component: () => import('../views/ComputerScience.vue'), meta: { title: '计算机科学 - mirai2cs' } },
-  { path: '/anime', name: 'Anime', component: () => import('../views/Anime.vue'), meta: { title: '动漫 - mirai2cs' } },
-  { path: '/japanese', name: 'Japanese', component: () => import('../views/Japanese.vue'), meta: { title: '日语 - mirai2cs' } },
-  { path: '/game', name: 'Game', component: () => import('../views/Game.vue'), meta: { title: '游戏 - mirai2cs' } },
-  { path: '/test', name: 'Test', component: () => import('../views/Test.vue'), meta: { title: '测试' } },
-  { path: '/neuroscience', name: 'Neuroscience', component: () => import('../views/Neuroscience.vue'), meta: { title: '脑科学 - mirai2cs' } },
-  { path: '/nihon', name: 'Nihon', component: () => import('../views/Nihon.vue'), meta: { title: '日本 - mirai2cs' } }
   { path: '/', name: 'Home', component: () => import('../views/core/Home.vue'), meta: { title: '首页 - mirai2cs' } },
   { path: '/about', name: 'About', component: () => import('../views/core/About.vue'), meta: { title: '关于 - mirai2cs' } },
   { path: '/history', name: 'History', component: () => import('../views/core/History.vue'), meta: { title: '网站日志 - mirai2cs' } },
