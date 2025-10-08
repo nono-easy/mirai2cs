2797e7139542511bfcd2c6bb720006593176d45a
nono <a40376491@126.com>
2025-10-08 20:29:58 +0800

    feat: 尝试修复部署后页面不显示问题

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -25,3 +26,4 @@ export default defineConfig({
     }
   }
 })
+
