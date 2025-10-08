7a1a024069dd315f159ed9fee5bb70e327a10d6c
nono <a40376491@126.com>
2025-10-08 20:36:49 +0800

    feat: 回退

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -12,4 +11,3 @@ export default defineConfig({
     }
   }
 })
-
